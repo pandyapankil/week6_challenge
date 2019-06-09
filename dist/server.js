89669d5d7bc78db72598ccca6e0a6631185bccd3
Pankil <pankil@paveai.com>
2019-06-09 19:59:38 +0530

    put db settings in db.js

--- dist/server.js
+++ dist/server.js
@@ -86,6 +86,18 @@
 /************************************************************************/
 /******/ ({
 
+/***/ "./src/server/db.js":
+/*!**************************!*\
+  !*** ./src/server/db.js ***!
+  \**************************/
+/*! exports provided: client, dbName, collectionName */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, \"client\", function() { return client; });\n/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, \"dbName\", function() { return dbName; });\n/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, \"collectionName\", function() { return collectionName; });\nconst MongoClient = __webpack_require__(/*! mongodb */ \"mongodb\").MongoClient;\nconst uri = \"mongodb+srv://pankil:pankiladmin@week5cluster-acske.mongodb.net/test?retryWrites=true&w=majority\";\nconst client = new MongoClient(uri, { useNewUrlParser: true });\nconst dbName = \"sample_airbnb\";\nconst collectionName = \"listingsAndReviews\";\n\n\n//# sourceURL=webpack:///./src/server/db.js?");
+
+/***/ }),
+
 /***/ "./src/server/server.js":
 /*!******************************!*\
   !*** ./src/server/server.js ***!
@@ -94,7 +106,7 @@
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var express__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! express */ \"express\");\n/* harmony import */ var express__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(express__WEBPACK_IMPORTED_MODULE_0__);\n/* harmony import */ var path__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! path */ \"path\");\n/* harmony import */ var path__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(path__WEBPACK_IMPORTED_MODULE_1__);\n\n\n\nconst app = express__WEBPACK_IMPORTED_MODULE_0___default()(),\n            DIST_DIR = __dirname,\n            HTML_PATH = path__WEBPACK_IMPORTED_MODULE_1___default.a.join(DIST_DIR, '../src/views/');\nconst MongoClient = __webpack_require__(/*! mongodb */ \"mongodb\").MongoClient;\nconst uri = \"mongodb+srv://pankil:pankiladmin@week5cluster-acske.mongodb.net/test?retryWrites=true&w=majority\";\nconst client = new MongoClient(uri, { useNewUrlParser: true });\nvar collection;\n\napp.set('views', HTML_PATH);\napp.set('view engine', 'pug');\n\napp.get('/abc', (req, res) => {\n    res.render('index')\n});\n\napp.get('/', async (req, res) => {\n    // res.render('index')\n    try {\n        const result = await collection.find({}).toArray();\n        res.send(result);\n    } catch (err) {\n        return res.status(500).send(error);\n    }\n});\n\nconst PORT = process.env.PORT || 3000;\napp.listen(PORT, () => {\n\n    client.connect((err, result) => {\n        if (err) {\n            throw err;\n        }\n        collection = client.db(\"sample_airbnb\").collection(\"listingsAndReviews\");\n    });\n\n    console.log(`App listening to ${PORT}....`)\n    console.log('Press Ctrl+C to quit.')\n});\n\n\n//# sourceURL=webpack:///./src/server/server.js?");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var express__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! express */ \"express\");\n/* harmony import */ var express__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(express__WEBPACK_IMPORTED_MODULE_0__);\n/* harmony import */ var path__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! path */ \"path\");\n/* harmony import */ var path__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(path__WEBPACK_IMPORTED_MODULE_1__);\n/* harmony import */ var _db__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./db */ \"./src/server/db.js\");\n\n\n\n// import client from './db';\n\nconst app = express__WEBPACK_IMPORTED_MODULE_0___default()(),\n            DIST_DIR = __dirname,\n            HTML_PATH = path__WEBPACK_IMPORTED_MODULE_1___default.a.join(DIST_DIR, '../src/views/');\nvar collection;\napp.set('views', HTML_PATH);\napp.set('view engine', 'pug');\n\napp.get('/abc', (req, res) => {\n    res.render('index')\n});\n\napp.get('/', async (req, res) => {\n    res.render('index')\n});\n\nconst PORT = process.env.PORT || 3000;\napp.listen(PORT, () => {\n\n    _db__WEBPACK_IMPORTED_MODULE_2__[\"client\"].connect(err => {\n        if (err) {\n            throw err;\n        }\n        collection = _db__WEBPACK_IMPORTED_MODULE_2__[\"client\"].db(_db__WEBPACK_IMPORTED_MODULE_2__[\"dbName\"]).collection(_db__WEBPACK_IMPORTED_MODULE_2__[\"collectionName\"]);\n    });\n\n    console.log(`App listening to ${PORT}....`)\n    console.log('Press Ctrl+C to quit.')\n});\n\n\n//# sourceURL=webpack:///./src/server/server.js?");
 
 /***/ }),
 
--- src/server/server.js
+++ src/server/server.js
@@ -1,14 +1,12 @@
 import express from 'express';
 import path from 'path';
+import {client, dbName, collectionName} from './db';
+// import client from './db';
 
 const app = express(),
             DIST_DIR = __dirname,
             HTML_PATH = path.join(DIST_DIR, '../src/views/');
-const MongoClient = require('mongodb').MongoClient;
-const uri = "mongodb+srv://pankil:pankiladmin@week5cluster-acske.mongodb.net/test?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true });
 var collection;
-
 app.set('views', HTML_PATH);
 app.set('view engine', 'pug');
 
@@ -17,23 +15,17 @@ app.get('/abc', (req, res) => {
 });
 
 app.get('/', async (req, res) => {
-    // res.render('index')
-    try {
-        const result = await collection.find({}).toArray();
-        res.send(result);
-    } catch (err) {
-        return res.status(500).send(error);
-    }
+    res.render('index')
 });
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
 
-    client.connect((err, result) => {
+    client.connect(err => {
         if (err) {
             throw err;
         }
-        collection = client.db("sample_airbnb").collection("listingsAndReviews");
+        collection = client.db(dbName).collection(collectionName);
     });
 
     console.log(`App listening to ${PORT}....`)
